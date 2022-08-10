a72ba315287dd2867afb758860ad3d3ccc808e22
Enzo Medaglia <c0643881@vale.com>
2022-08-10 15:29:26 -0300

    begin of development for recipes API, everything before is to setup django, user login, database, tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,6 @@
 """
 Django admin customization
 """
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -45,3 +44,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
